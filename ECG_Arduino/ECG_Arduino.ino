80f54a2dafccc8cfa543ae258b3adb59729d1808
Mauro-CVO <mauo.cortes2@gmail.com>
2022-01-27 16:37:53 -0600

    Digitilazación de la señal del ECG

---
+++ ECG_Arduino/ECG_Arduino.ino
@@ -0,0 +1,19 @@
+
+float signal_ecg; // Crea Variable tipo float
+
+void setup(){
+// Inicializamos comunicacion serial con el microcontrolador
+  Serial.begin(9600); 
+  
+}
+void loop() {
+// Leemos la señal del ECG y la asignamos a signal_ecg
+  signal_ecg = analogRead(A0); 
+// Mapeamos el valor de signal_ecg de 0-1024 a 0-5
+  signal_ecg = (signal_ecg/1024.0)*5; //
+//Imprimimos signal_ecg
+  Serial.println(signal_ecg);
+//Delay de 10 microsegundos
+  delay(10);
+
+}
